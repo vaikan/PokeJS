21ac729ff2f54b20286c9eb8a1f67e2603a182d3
Saurav Pokhrel <sauravpokhrel.sp@gmail.com>
2016-01-21 17:39:33 +0100

    daily updates #1
    added function to get all the game versions
    changed readme description slightly

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PokeJS
-A simple JavaScript wrapper for PokeAPI
+A simple Pokedex created using JavaScript and [PokeAPI] (http://pokeapi.co).
+
+**NOTE:**
+This is a work in progress. Contains bugs. 
--- src/js/gamever.js
+++ src/js/gamever.js
@@ -1,34 +1,42 @@
-function Versions(name, generation, year, uri) {
-  this.name = name;
-  this.generation = generation;
-  this.year = year;
-  this.uri = uri;
+function Versions() {
+  this.getVersionDetails = function (id, callback) {
+    var url = 'http://pokeapi.co/api/v1/game/'+id;
+    var parsedResponse;
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = getVersion;
+    httpRequest.open('GET', url);
+    httpRequest.send();
 
-  getVersionDetails = function() {
-    // TODO: Do something with versiondetails
+    function getVersion() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          var response = httpRequest.response;
+          parsedResponse  = JSON.parse(response);
+          callback(parsedResponse);
+          } else {
+          var data = 'Could not get the result';
+          console.log(data);
+        }
+      }
+    }
+  };
+
+  this.getAllVersionNames = function () {
+    var callbackFunction = function (parsedResponse) {
+      var obj = {
+        name: parsedResponse.name,
+        year: parsedResponse.release_year,
+        uri: parsedResponse.resource_uri
+      };
+
+      console.log(obj);
+    };
+    for (var i = 1; i <= 25; i++) {
+      this.getVersionDetails(i, callbackFunction);
+    }
+
   };
 }
 
-function getDetails() {
-  var count = 1;
-  var url = 'http://pokeapi.co/api/v1/game/'+count;
-  var parsedResponse;
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = getVersion;
-  httpRequest.open('GET', url);
-  httpRequest.send();
-
-  function getVersion() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        var response = httpRequest.response;
-        parsedResponse  = JSON.parse(response);
-        console.log(parsedResponse);
-        } else {
-        var data = 'Could not get the result';
-        console.log(data);
-      }
-    }
-  }
-
-}
+var gamever = new Versions();
+gamever.getAllVersionNames();
