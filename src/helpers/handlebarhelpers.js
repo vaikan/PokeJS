21d0747e475a68e7b43d9670cbed199919ef56d6
Saurav Pokhrel <sauravpokhrel.sp@gmail.com>
2020-11-22 22:22:45 +0100

    - fetch API to get Pokemon data, - fixed text format

--- src/helpers/handlebarhelpers.js
+++ src/helpers/handlebarhelpers.js
@@ -1,46 +1,83 @@
+const toUpperCase = (data) => {
+  let firstLetter;
+  let otherLetters;
+  let capitalCase;
+  let stringArr = [];
+
+  const stringSplit = data.split("-");
+
+  for (let a in stringSplit) {
+    if (stringSplit[a].match(/^(i|ii|iii|iv|v|vi)$/)) {
+      capitalCase = stringSplit[a].toUpperCase();
+      stringArr.push(capitalCase);
+    } else {
+      firstLetter = stringSplit[a].substr(0, 1);
+      otherLetters = stringSplit[a].substr(1, stringSplit[a].length);
+      capitalCase = firstLetter.toUpperCase();
+
+      var newString = capitalCase + otherLetters;
+      stringArr.push(newString);
+    }
+  }
+
+  return stringArr;
+};
+
+const formatString = (stringArray, formatter) => {
+  let correctedCase;
+
+  for (let i = 0; i < stringArray.length; i++) {
+    if (i === 0) {
+      correctedCase = stringArray[i];
+    } else {
+      correctedCase = correctedCase + formatter + stringArray[i];
+    }
+  }
+
+  return correctedCase;
+};
+
 /**
  * FirstStringUpperCase - changes first character of the string to upper case except the Roman numericals which are all changed to upper case
  * @param  {string} FirstStringUpperCase      name of the helper
  * @param  {Function} function(data)          callback function with data as parameter
  * @return {string} correctedCase             uppercased string
  */
-Handlebars.registerHelper("FirstStringUpperCase", function(data) {
-  var correctedCase;
-  var stringArr = [];
-  var firstLetter;
-  var otherLetters;
-  var capitalCase;
+Handlebars.registerHelper("FirstStringUpperCase", (data) => {
+  let stringArr = [];
+  let correctedCase;
 
   if (data) {
-    if (data.search('-') != -1) {
-      var stringSplit = data.split('-');
-      for (var a in stringSplit) {
+    stringArr = toUpperCase(data);
 
-        if (stringSplit[a].match(/^(i|ii|iii|iv|v|vi)$/)) {
-          capitalCase = stringSplit[a].toUpperCase();
-          stringArr.push(capitalCase);
-        } else {
-          firstLetter = stringSplit[a].substr(0,1);
-          otherLetters = stringSplit[a].substr(1, stringSplit[a].length);
-          capitalCase = firstLetter.toUpperCase();
+    correctedCase = formatString(stringArr, " ");
 
-          var newString = capitalCase+otherLetters;
-          stringArr.push(newString);
-        }
-      }
-      if (stringArr.length === 2) {
-        correctedCase = stringArr[0]+'-'+stringArr[1];
-      } else if (stringArr.length === 3) {
-        correctedCase = stringArr[0]+'-'+stringArr[1]+'-'+stringArr[2];
-      } else {
-        correctedCase = stringArr[0]+'-'+stringArr[1]+'-'+stringArr[2]+'-'+stringArr[3];
-      }
+    return correctedCase;
+  }
+});
+
+Handlebars.registerHelper("FirstStringUpperCaseTitle", (data) => {
+  let stringArr = [];
+  let correctedCase;
+
+  if (data) {
+    stringArr = toUpperCase(data);
+
+    if (stringArr.length === 1) {
+      correctedCase = stringArr[0];
+    } else if (stringArr.length === 2) {
+      correctedCase = stringArr[0] + ", " + stringArr[1];
+    } else if (stringArr.length === 3) {
+      correctedCase = stringArr[0] + "-" + stringArr[1] + "-" + stringArr[2];
     } else {
-      firstLetter = data.substr(0,1);
-      otherLetters = data.substr(1, data.length);
-      capitalCase = firstLetter.toUpperCase();
-
-      correctedCase = capitalCase+otherLetters;
+      correctedCase =
+        stringArr[0] +
+        " " +
+        stringArr[1] +
+        ", " +
+        stringArr[2] +
+        " " +
+        stringArr[3];
     }
     return correctedCase;
   }
